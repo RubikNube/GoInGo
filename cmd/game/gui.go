e878ba480e6e8778b61d202f619b07877671c65a
RubikNube <roland@kiesendahl.de>
2025-06-26 10:29:04 +0200

    replaced box chars by simple ascii chars for simplicity

--- cmd/game/gui.go
+++ cmd/game/gui.go
@@ -21,7 +21,7 @@ type Gui struct {
 }
 
 var FieldStateName = map[FieldState]string{
-	Empty: "┼",
+	Empty: "+",
 	Black: "○", // Unicode empty circle for black stone
 	White: "●", // Unicode filled circle for white stone
 }
@@ -55,68 +55,38 @@ func (g *Gui) Refresh() {
 func (g *Gui) DrawGridToWriter(w io.Writer, cursorRow, cursorCol int) {
 	// Column labels
 	fmt.Fprint(w, "   ")
-	for j := 0; j < 9; j++ {
+	for j := range g.Grid[0] {
 		fmt.Fprintf(w, " %c  ", 'A'+j)
 	}
 	fmt.Fprintln(w)
 
-	for i := 0; i < 9; i++ {
+	for i, row := range g.Grid {
 		// Row label
 		fmt.Fprintf(w, "%2d ", i+1)
-		for j := 0; j < 9; j++ {
-			stone := g.Grid[i][j].String()
-			// Use box-drawing characters for borders and intersections
-			if g.Grid[i][j] != Empty {
-				stone = g.Grid[i][j].String()
-			} else {
-				switch {
-				case i == 0 && j == 0:
-					stone = "┌"
-				case i == 0 && j == 8:
-					stone = "┐"
-				case i == 8 && j == 0:
-					stone = "└"
-				case i == 8 && j == 8:
-					stone = "┘"
-				case i == 0:
-					stone = "┬"
-				case i == 8:
-					stone = "┴"
-				case j == 0:
-					stone = "├"
-				case j == 8:
-					stone = "┤"
-				default:
-					stone = g.Grid[i][j].String()
-				}
-			}
-			var cell string
-			if j == 0 {
-				cell = fmt.Sprintf(" %s─", stone)
-			} else if j == 8 {
-				cell = fmt.Sprintf("─%s ", stone)
-			} else {
-				// Use a box-drawing character for the stone
-				cell = fmt.Sprintf("─%s─", stone)
-			}
+		for j, cellVal := range row {
+			stone := cellVal.String()
 
+			cell := fmt.Sprintf("-%s-", stone)
+			if j == 0 {
+				cell = fmt.Sprintf(" %s-", stone)
+			} else if j == 8 {
+				cell = fmt.Sprintf("-%s ", stone)
+			}
 			if i == cursorRow && j == cursorCol {
-				// Use a different background or brackets, but keep width 3
 				cell = fmt.Sprintf("[%s]", stone)
 			}
 			fmt.Fprint(w, cell)
-
 			// Draw horizontal line except after last column
 			if j < 8 {
-				fmt.Fprint(w, "─")
+				fmt.Fprint(w, "-")
 			}
 		}
 		fmt.Fprintln(w)
 		// Draw vertical lines except after last row
 		if i < 8 {
 			fmt.Fprint(w, "   ")
-			for j := 0; j < 9; j++ {
-				fmt.Fprint(w, " │ ")
+			for j := range g.Grid[i] {
+				fmt.Fprint(w, " | ")
 				if j < 8 {
 					fmt.Fprint(w, " ")
 				}
